9d0712f7d41374cf14ba1e538012b3455d148c27
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-10-14 21:33:50 +0300

    first try code 05

---
+++ python/linked_list/README.md
@@ -0,0 +1,15 @@
+# Singly Linked List
+created a linked list.
+## Challenge
+write a linked list having head value with an O(1) and includes methods (insert, inclueds and to string)
+## Approach & Efficiency
+O(1) time performance.
+
+## API
+
+insert : Adds a new node with that value to the head of the list with an O(1) Time performance.
+
+includes : Indicates whether that value exists as a Node’s value somewhere within the list.
+
+to string : return a string representing all the values in the Linked List, formatted as:
+"{ a } -> { b } -> { c } -> NULL"
--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,84 @@
+class Node:
+  """
+  A class representing a Node
+
+  Attributes
+  ----------
+
+
+  Methods
+  -------
+  __init__(data, next_):
+      the constructor method for the class, it takes two parameters, the data parameter is the a reference to the data the node will hold, and the next_ 
+
+  """
+
+  def __init__(self, data, next_ = None):
+    self.data = data
+    self.next_ = next_
+
+    
+
+    
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+  """
+  A class for creating instances of a Linked List.
+  
+  Data and other attributes defined here:
+  
+  head: Node | None
 
-    def __init__(self):
-        # initialization here
-        pass
+  Methods defined here
 
-    def some_method(self):
-        # method body here
-        pass
+  insert(value: any)
+  contains(value: any) -> bool
+  """
+
+  def __init__(self):
+    self.head = None
+
+    """"
+    Insert creates a Node with the value that was passed and adds
+    it to the head of the linked list shifting all other values down
+
+    arguments:
+    value : any
+
+    returns: None
+    """
+  
+    def insert (self, data):
+        new_node = Node (data, self.head)
+        self.head = new_node
+        self.size += 1
+
+    def includes (self, data):
+        this_node = self.head
+        while this_node:
+            if this_node.get_data() == data:
+                return True
+            else:
+                this_node = this_node.get_next()
+        return False
+
+
+    def __str__(self):
+        if self.head:
+            saved_data = "{"f"{self.head.data}""} "
+            this_node = self.head
+            while this_node:
+                this_node = this_node.next_node
+                if  this_node:
+                    saved_data += "-> ""{"f"{ this_node.data}""} "
+                else:
+                    saved_data += "-> NULL"
+        else:
+            return "Your List still empty"
+        return saved_data
+
+
+myList = LinkedList()
+print(myList.includes(5))
+print(myList.includes(3))
+print(myList.__str__())
