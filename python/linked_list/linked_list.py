6c5593f46bad38b4b2462215a707ad0c23e4dfa0
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-10-19 17:54:57 +0300

    editing

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,16 +1,12 @@
 class Node:
   """
   A class representing a Node
-
   Attributes
   ----------
-
-
   Methods
   -------
   __init__(data, next_):
       the constructor method for the class, it takes two parameters, the data parameter is the a reference to the data the node will hold, and the next_ 
-
   """
 
   def __init__(self, data, next_ = None):
@@ -28,9 +24,7 @@ class LinkedList:
   Data and other attributes defined here:
   
   head: Node | None
-
   Methods defined here
-
   insert(value: any)
   contains(value: any) -> bool
   """
@@ -41,53 +35,45 @@ class LinkedList:
     """"
     Insert creates a Node with the value that was passed and adds
     it to the head of the linked list shifting all other values down
-
     arguments:
     value : any
-
     returns: None
     """
   
-    def insert(self, data):
+  def insert(self, data):
         """
         insert a node in the list
-
         """
-        new_node = Node (data, self.head)
-        new_node.next_=self.head
+        new_node = Node(data,self.head)
         self.head=new_node
 
-    def includes(self, data):
+  def includes(self, data):
         """
         check if the value exist in the list
         return a boolean: True if exist, False if not
-
         """
         if self.head is None:
             return False
         else:
             item=self.head
             while(item):
-                if(item.value==data):
+                if(item.data==data):
                     return True
-                else: 
-                    item=item.next_
-            
-            return False
 
+                item=item.next_
 
-    def __str__(self):
+            return False 
+                    
+  def to_string(self):
 
         """
         return a string of the list
-
         """
 
         item=self.head
         space=''
         if item !=None:
                 while(item):
-                    space+=' {'+str(item.value)+'} ->'
+                    space+='{'+str(item.data)+'}->'
                     item=item.next_
                 return space+'NULL'
-
