395b80b809f0e2aadcd86fee41714c8e5829e221
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-12-09 05:18:37 +0200

    fix problems

--- python/code_challenges/hashmap_left_join/hashmap_left_join.py
+++ python/code_challenges/hashmap_left_join/hashmap_left_join.py
@@ -1,11 +1,70 @@
+class HashTable():
+  def __init__(self ,size = 1024):
+    """initialization hash table"""
+    self.max = size 
+    self.arr = [[] for i in range(self.max)] 
+    
 
-def hashmap_left_join(key, key2):
+  def get_hash(self, key):
+    """function to return the hash value using ASCII code"""
+    h = 0
+    for char in key:
+        h += ord(char)
+    hash_index= h % self.max 
+    return hash_index
+ 
+  def add(self ,key ,value):
+    """Function the store key value pairs in the key index of list"""
+    h = self.get_hash(key)
+    found = False
+  
+    for idx, element in enumerate(self.arr[h]):
+      if len(element)==2 and element[0] == key:
+          self.arr[h][idx] = (key,value)
+          found = True
+    if not found: #
+      self.arr[h].append((key,value))
 
-    result = []
+  def get(self, key):
+    """function that return the value stored in the key index"""
+    h = self.get_hash(key)
+    for element in self.arr[h]:
+      if element[0] == key:
+        return element[1] 
 
-    for i in key.keys():
-        if i in key2.keys():
-            result.append([i, key[i], key2[i]])
+  def contains(self, key):
+    h = self.get_hash(key)
+    found = False
+    
+    for idx, element in enumerate(self.arr[h]):
+      if len(element)==2 and element[0] == key:
+        found = True
+    return found
+
+  def delete(self , key):
+    """function that set the value of key to None"""
+    h = self.get_hash(key)
+    for idx , element in enumerate(self.arr[h]):
+      if element[0] == key:
+        del self.arr[h][idx]
+
+  def keys(self):
+    """function that return all keys in the hash table"""
+    keys = []
+    for element in self.arr:
+        for key in element:
+            keys.append(key[0])
+    return keys
+
+def left_join_hash(ht_one,ht_two):
+    output = []
+    for key in ht_one.keys():
+        if key in ht_two.keys():
+            output.append([key, ht_one.get(key), ht_two.get(key)])
         else:
-            result.append([i, key[i], "None"])
-    return result
+            output.append([key, ht_one.get(key), None])
+            
+    for key in ht_two.keys():
+        if key not in ht_one.keys():
+            output.append([key, None, ht_two.get(key)])
+    return output
--- python/tests/test_hashmap_left_join.py
+++ python/tests/test_hashmap_left_join.py
@@ -1,17 +1,16 @@
-from code_challenges.hashmap_left_join.hashmap_left_join import hashmap_left_join
+from code_challenges.hashmap_left_join.hashmap_left_join import *
 
-def test_left_join():
-    actual = hashmap_left_join(
-        key={
-        'first': 'Forza',
-        'second': 'Amunt',
-        'third': 'Go',
-        'fourth': 'viva',
-    }, key2={
-        'first': 'Milan',
-        'second': 'Valencia',
-        'third': 'Hilal',
-        'seven': 'algeria',
-    })
-    excepted = [['first', 'Forza', 'Milan'], ['second', 'Amunt', 'Valencia'], ['third', 'Go', 'Hilal'], ['fourth', 'viva', 'None']]
-    assert actual == excepted
+def test_left_join_hash():
+  ht_one = HashTable()
+  ht_one.add("fond", "enamored")
+  ht_one.add("wrath", "anger")
+  ht_one.add("diligent", "employed")
+  ht_one.add("outfit", "garb")
+  ht_one.add("guide", "usher")
+  ht_two = HashTable()
+  ht_two.add("fond", "averse")
+  ht_two.add("wrath", "delight")
+  ht_two.add("diligent", "idle")
+  ht_two.add("guide", "follow")
+  ht_two.add("flow", "jam")
+  assert (left_join_hash(ht_one,ht_two)) == [['fond', 'enamored', 'averse'], ['guide', 'usher', 'follow'], ['wrath', 'anger', 'delight'], ['outfit', 'garb', None], ['diligent', 'employed', 'idle'], ['flow', None, 'jam']]
